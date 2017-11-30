363d76fe3a1142d34b625f4e1463f839c73f1e0f
Jairo Velasquez <jairo.j5180@gmail.com>
2017-11-30 14:26:53 -0600

    Migration
    -Tabla provider.
    -Tabla client.

---
+++ database/migrations/2017_11_29_163412_create_client_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateClientTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('clientes', function (Blueprint $table) {
+            $table->increments('id_cliente');
+            $table->string('nombre');
+            $table->string('apellido');
+            $table->string('direccion');
+            $table->string('email');
+            $table->string('NIT')->default('null');
+            $table->text('telefono');
+            $table->date('fecha_nacimiento');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('clientes');
+    }
+}
